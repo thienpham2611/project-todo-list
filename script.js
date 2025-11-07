5d037df8e8c6c25339c48eeeefdcde49bc3e4ed0
khoi <dangkhoi93hv@gmail.com>
2025-11-07 14:37:10 +0700

    Fix: Áp dụng early return theo review

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// Đợi DOM load xong
-document.addEventListener('DOMContentLoaded', function() {
-const addButton = document.getElementById('add-task-btn');
-const taskInput = document.getElementById('new-task-input');
-const taskList = document.getElementById('task-list');
+JavaScript
+// ... (code ở trên)
 addButton.addEventListener('click', function() {
 const taskText = taskInput.value;
-if (taskText.trim() !== "") {
+// Áp dụng "early return"
+if (taskText.trim() === "") {
+alert("Vui lòng nhập nội dung!"); // Thêm tí bonus
+return;
+}
 // Tạo một <li> mới
 const newTask = document.createElement('li');
 newTask.textContent = taskText;
@@ -13,6 +14,5 @@ newTask.textContent = taskText;
 taskList.appendChild(newTask);
 // Xóa nội dung input
 taskInput.value = "";
-}
 });
-});
+// ... (code ở dưới)
